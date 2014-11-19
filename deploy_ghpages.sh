cad97cbe57c670c4e932e6b8cdbaef7471cb6f0d
ptsurko <pavel.tsurko@gmail.com>
2014-11-18 21:52:43 -0800

    fixed urls pathes

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -8,10 +8,8 @@ if [ $TRAVIS_PULL_REQUEST != false ];
 fi
 
 (
-  # git init
+  echo "Pushing build to ${GH_REF} gh-pages branch."
   git config user.name "Travis-CI"
   git config user.email "travis@nodemeatspace.com"
-  # git add -v .
-  # git commit -v -m "Deployed to Github Pages"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
