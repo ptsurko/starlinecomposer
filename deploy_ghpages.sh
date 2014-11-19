54c01f2c61d56cbb747a9ac4c679f5218790ee5f
ptsurko <pavel.tsurko@gmail.com>
2014-11-18 22:07:02 -0800

    updated script to install npm/bower dependencies

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -9,7 +9,10 @@ fi
 
 (
   echo "Pushing build to ${GH_REF} gh-pages branch."
+  # git init
   git config user.name "Travis-CI"
   git config user.email "travis@nodemeatspace.com"
+  git add -v .
+  git commit -v -m "Deployed to Github Pages"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
